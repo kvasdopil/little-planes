f7330c20adfb085b9b926e5fb1beb75b811b6188
Alexey Guskov <alexey@nextml.com>
2025-03-05 20:29:44 +0100

    Add hover animation to City component using react-spring

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -1,5 +1,7 @@
 import { Sphere } from '@react-three/drei';
+import { useSpring, animated } from '@react-spring/three';
 import { geoToCartesian } from '../utils/coordinates';
+import { useState } from 'react';
 
 interface CityProps {
   name: string;
@@ -12,12 +14,24 @@ const EARTH_RADIUS = 2;
 
 export function City({ latitude, longitude, size }: CityProps) {
   const position = geoToCartesian(latitude, longitude, EARTH_RADIUS);
+  const [hovered, setHovered] = useState(false);
+  
+  const springs = useSpring({
+    scale: hovered ? 1.3 : 1,
+    config: { mass: 2, tension: 400, friction: 30 }
+  });
 
   return (
     <group position={position}>
-      <Sphere args={[size, 16, 16]}>
-        <meshBasicMaterial color="white" />
-      </Sphere>
+      <animated.mesh
+        scale={springs.scale}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+      >
+        <Sphere args={[size, 16, 16]}>
+          <meshBasicMaterial color="white" />
+        </Sphere>
+      </animated.mesh>
     </group>
   );
 }
